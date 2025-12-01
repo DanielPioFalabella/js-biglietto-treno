dc48f6053699b9367d916512fba008d4e96fcebd
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-01 15:45:06 +0100

    modifiche prezzi

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// quanti km vuole percorrere il passeggero
+let numKm = parseInt(prompt("quanti km vuoi percorrere?"))
+console.log("quanti km vuoi percorrere?", numKm);
+
+// età del passeggero
+let etaPasseggero = parseInt(prompt("qual è la tua età?"))
+console.log("qual è la tua età?", etaPasseggero);
+
+
+// prezzo totale del biglietto per il viaggio
+const bigliettoBase = numKm * 0.21;
+console.log(bigliettoBase);
+
+// sconto del biglietto in base all'età del passeggero
+if (etaPasseggero < 18) {
+    bigliettoFinale = bigliettoBase * 0.8;
+    console.log("prezzo del biglietto", bigliettoFinale)
+} else if (etaPasseggero >= 65) {
+    bigliettoFinale = bigliettoBase * 0.6;
+    console.log("prezzo del biglietto", bigliettoFinale);
+    
+} else {
+    bigliettoFinale = bigliettoBase;
+    console.log("prezzo del biglietto", bigliettoFinale);
+}
+
+// OUTPUT
--- script.js
+++
@@ -1,8 +0,0 @@
-// quanti km vuole percorrere il passeggero
-const kmPasseggero = prompt("quanti km vuoi percorrere?")
-// età del passeggero
-
-// prezzo totale del biglietto per il viaggio
-// sconto del biglietto in base all'età del passeggero
-
-// OUTPUT
